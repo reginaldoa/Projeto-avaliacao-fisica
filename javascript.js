c9ac58f0cd7327876ca0dbb782c82308a5b0981f
Reginaldo Alves <reginaldoalvesti@oulookt.com>
2022-08-11 18:28:01 -0300

    config nomes

--- javascript.js
+++ javascript.js
@@ -9,13 +9,13 @@ class Enviar{
     resposta(){
         this.button.addEventListener("click",e =>{
             e.preventDefault()
+            this.nome = document.querySelector("#nome").value;
             const res =document.querySelector("#res")
             return res.innerHTML=`${this.nome}, o seu imc é de ${this.imc().toFixed(2)}`
         })
     }
 
     imc(){
-        this.nome = document.querySelector("#nome").value;
         this.idade = document.querySelector("#idade").value;
         this.peso = document.querySelector("#peso").value;
         this.altura = document.querySelector("#altura").value;
@@ -25,9 +25,11 @@ class Enviar{
         this.alturacomVIRGULA = parseFloat(this.alturafinal.replace(',','.'));
         let respostaimc = this.pesocomVIRGULA/(this.alturacomVIRGULA*this.alturacomVIRGULA);
 
-        if(!Number(this.pesocomVIRGULA) || (!Number(this.alturacomVIRGULA)) || this.idade===''){ alert('Todos os campos devem ser preenchidos. Altura e peso devem ser preenchidos sem pontos ou vírgulas')}
+        if(this.nome===null||!Number(this.pesocomVIRGULA) || (!Number(this.alturacomVIRGULA)) || this.idade===''){ alert('Todos os campos devem ser preenchidos. Altura e peso devem ser preenchidos sem pontos ou vírgulas')}else{
+            return respostaimc;
+        }
 
-        return respostaimc;
+        
     }
 
     sexo(){
